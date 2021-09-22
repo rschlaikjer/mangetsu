de2cd8ea16a75054e8b5debe833577914c734432
Ross Schlaikjer <ross@schlaikjer.net>
2021-09-21 22:01:37 -0400

    Allow specifying output dir for mzp extract

--- src/tools/mzp_extract.cpp
+++ src/tools/mzp_extract.cpp
@@ -2,9 +2,11 @@
 #include <mg/util/fs.hpp>
 #include <mg/util/string.hpp>
 
+#include <filesystem>
+
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "%s infile\n", argv[0]);
+  if (argc != 2 && argc != 3) {
+    fprintf(stderr, "%s infile [out_dir]\n", argv[0]);
     return -1;
   }
 
@@ -21,14 +23,30 @@ int main(int argc, char **argv) {
     return -1;
   }
 
+  // If we are putting output in a specific folder, check it exists
+  if (!std::filesystem::exists(argv[2])) {
+    fprintf(stderr, "Output directory '%s' does not exist, exiting\n", argv[2]);
+    return -1;
+  }
+
+  // Helper to get output file path for entries
+  auto output_path = [&](unsigned entry) -> std::string {
+    std::string file_name = mg::string::format("%s_%04u.bin", argv[1], entry);
+    if (argc == 3) {
+      return std::filesystem::path(argv[2]).append(file_name);
+    } else {
+      return std::filesystem::path(file_name);
+    }
+  };
+
   // Split MZP into constituent files
   for (unsigned i = 0; i < mzp.entry_headers.size(); i++) {
-    std::string output_filename = mg::string::format("%s_%04u.bin", argv[1], i);
+    std::string path = output_path(i);
     auto &data = mzp.entry_data[i];
-    if (!mg::fs::write_file(output_filename.c_str(), data)) {
+    if (!mg::fs::write_file(path.c_str(), data)) {
       return -1;
     }
-    fprintf(stderr, "Wrote %s\n", output_filename.c_str());
+    fprintf(stderr, "Wrote %s\n", path.c_str());
   }
 
   return 0;
