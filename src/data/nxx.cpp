2c45a67de76dc59a341c59f191151fdf1af7cc6e
Ross Schlaikjer <ross@schlaikjer.net>
2024-01-18 07:49:31 +0900

    Better check

--- src/data/nxx.cpp
+++ src/data/nxx.cpp
@@ -124,16 +124,15 @@ bool nxcx_decompress(const Nxx &header, const uint8_t *data, std::string &out) {
   // Perform inflation
   inflateInit(&istream);
   const int err = inflate(&istream, Z_FINISH);
-  if (err != Z_STREAM_END) {
-    if (err == Z_OK) {
-      fprintf(stderr,
-              "zlib error: did not reach end of stream, need more data?\n");
-    } else {
-      fprintf(stderr, "zlib error: %d: %s\n", err, istream.msg);
-    }
+  if (err != Z_STREAM_END && err != Z_OK) {
+    fprintf(stderr, "zlib error: %d: %s\n", err, istream.msg);
     return false;
   }
-  inflateEnd(&istream);
+  const int err2 = inflateEnd(&istream);
+  if (err2 != Z_OK) {
+    fprintf(stderr, "zlib error: %d: %s\n", err, istream.msg);
+    return false;
+  }
 
   return true;
 }
